29d6844ef5aa8bf1b581e695843a5243cda20ac0
vishalhandoo <81742803+vishalhandoo@users.noreply.github.com>
2023-07-13 19:56:44 +0530

    Add files via upload

---
+++ Lecture30.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+class Queue{
+
+    public:
+    int queue[1000];
+    int capacity;
+    int front;
+    int rear;
+
+    Queue(){
+        this->front = 0;
+        this->rear = -1;
+        this->capacity=1000;
+    }
+
+};
+
+
+int main(){
+
+    return 0;
+}
